ea171230d7b345774f7420d2e18971911b3931d9
Brendan Chou <brendanchou@msn.com>
2019-08-01 15:49:57 -0700

    test local 0.0.3

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-import os
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 REQUIREMENTS = [
     'requests>=2.5',
     'six==1.12',
     'web3==4.9.2',
     'pytest>=4.4.0,<5.0.0',
     'tox==3.13.2',
-    'setuptools',
+    'setuptools==41.0.1',
     'eth_keys'
 ]
 
+print(LONG_DESCRIPTION)
+
 setup(
     name='dydx-python',
-    version='0.0.4',
+    version='0.0.3',
     packages=find_packages(),
     description='dYdX Python REST API for Limit Orders',
-    long_description=README,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     url='https://github.com/dydxprotocol/dydx-python',
     author='dYdX Trading Inc.',
