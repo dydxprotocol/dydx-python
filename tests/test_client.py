b4e182187bdfe930d9c91358ad41aefa2594a508
Brendan Chou <brendanchou@msn.com>
2020-07-23 13:21:02 -0700

    fix test

--- tests/test_client.py
+++ tests/test_client.py
@@ -48,7 +48,7 @@ def _create_solo_order_matcher(client, args):
         assert order['amount'] == str(args['amount'])
         assert order['limitPrice'] == str(args['limitPrice'])
         assert order['triggerPrice'] == '0'
-        assert order['limitFee'] == '0.005'
+        assert order['limitFee'] == '0.01'
         assert order['makerAccountOwner'] == \
             client.public_address
         assert order['makerAccountNumber'] == \
