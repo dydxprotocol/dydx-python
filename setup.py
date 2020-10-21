9ccb636603e65722f41b5f58cc41951d9560e3ec
Filip Małachowicz <filip@makerdao.com>
2020-10-21 14:06:23 +0200

    Update web3 and eth-account dependencies

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ REQUIREMENTS = [
 
 setup(
     name='dydx-python',
-    version='0.11.1',
+    version='0.11.2',
     packages=find_packages(),
     package_data={
         'dydx': ['abi/*.json'],
